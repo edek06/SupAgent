66a3b1a0ba1eed4f45b0c92158ee04dab1aadbf3
gwedi <Wozniak.Grzegorz@imes-icore.de>
2023-02-10 22:32:28 +0100

    First stap works!

--- ticketportal/views.py
+++ ticketportal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from .models import Ticket
 def tickets(request):
-    tickets = Ticket.all()
-    return render(request, 'ticketportal/ticketportal/tickets.html', {'tickets': tickets})
+    tickets = Ticket.objects
+    return render(request, 'ticketportal/tickets.html', {'tickets': tickets})
