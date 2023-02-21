a5f9f12d991e9262ebc165ab21dc4fccd6f1ae91
gwedi <Wozniak.Grzegorz@imes-icore.de>
2023-02-22 00:08:41 +0100

    TicketForm in forms + create_ticket in views & html added

--- ticketportal/urls.py
+++ ticketportal/urls.py
@@ -4,4 +4,5 @@ app_name = 'ticketportal'
 urlpatterns = [
     path('', views.tickets, name='tickets'),
     path('<int:ticket_id>', views.ticket_detail, name='ticket_detail'),
+    path('create_ticket/', views.create_ticket, name='create_ticket'),
 ]
--- ticketportal/views.py
+++ ticketportal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Ticket
+from .forms import TicketForm
 def tickets(request):
     # all tickets save in 'tickets'
     tickets = Ticket.objects.all()
@@ -12,3 +13,22 @@ def ticket_detail(request, ticket_id):
     ticket = get_object_or_404(Ticket, pk=ticket_id)
     # return this page and the ticket
     return render(request, 'ticketportal/ticket_detail.html', {'ticket': ticket})
+
+def create_ticket(request):
+    ticket = None
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TicketForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # Create a Ticket object without saving it to the database
+            ticket = form.save(commit=False)
+            # Save the comment to the database
+            ticket.save()
+    # if a GET we'll create a blank form
+    else:
+        form = TicketForm()
+    return render(request, 'ticketportal/create_ticket.html',
+                  {'form': form,
+                   'ticket': ticket})
