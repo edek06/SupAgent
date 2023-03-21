4d95a11633956ac5a23b71ccb16161d85bf5bf24
gwedi <Wozniak.Grzegorz@imes-icore.de>
2023-03-21 15:58:49 +0100

    Employees Page created and a list of all works

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-app_name = 'ticketportal'
+app_name = 'employees'
 urlpatterns = [
     path('', views.show_employees, name='employees'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee
 from django.contrib.auth.decorators import login_required
 
 def show_employees(request):
-    return render(request, 'employees/employees.html')
+    employees = Employee.objects.all()
+    return render(request, 'employees/employees.html', {'employees':employees})
