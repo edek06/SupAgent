5a22a98ce0f1e5334cbf9937dba991444ab9934f
gwedi <Wozniak.Grzegorz@imes-icore.de>
2023-03-21 15:27:53 +0100

    model Employee created

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Employee
+# it's the same like 'admin.site.register(Employee)' - model Ticket registrieren für Admin-Page
+@admin.register(Employee)
+#now we can self change this look! (127.0.0.1:8000/admin)
+class EmployeeAdmin(admin.ModelAdmin):
+    # this parameter will be display
+    list_display = ['id', 'name', 'joined']
+    # we can filter all posts
+    list_filter = ['id', 'name', 'joined']
+    # this will be can find
+    search_fields = ['name']
+    date_hierarchy = 'joined'
+    ordering = ['joined']
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# we need this model to create all employees. For this users we can create a Ticket.
+# It's not the same like accounts - employees can not be an owner - only an end user
+class Employee(models.Model):
+    # Name and Surname of this employee
+    name = models.CharField(max_length=50)
+    # a date, when this user was created
+    joined = models.DateTimeField(auto_now_add=True)
+
+    # all Employees will be sort by joined (created) field
+    class Meta:
+        ordering = ['joined']
+        indexes = [
+            models.Index(fields=['joined']),
+        ]
+
+    # return a name of this employee
+    def __str__(self):
+        return self.name
